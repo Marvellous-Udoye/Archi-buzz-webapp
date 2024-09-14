2ef480cb42e22d33cb9bb9ce49118bfa6878d451
Marvellous-Udoye <155150860+Marvellous-Udoye@users.noreply.github.com>
2024-09-14 15:27:23 +0100

    feat: built courseCard component and fetched courses data from coursesData.json

---
+++ src/app/component/archi-courses.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import CourseCard from "./common/archi-course-card";
+import { ArchiCorurseProps } from "./common/archi-course-card";
+
+const ArchiCourses = () => {
+  const [courses, setCourses] = useState<ArchiCorurseProps[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  const fetchCoursesData = async () => {
+    try {
+      const response = await fetch('/coursesData.json')
+      if (!response.ok) {
+        throw new Error('Unable to fecth courses data')
+      }
+      const courseData = await response.json()
+      setCourses(courseData)
+    }
+    catch (e) {
+      setError('Reload Page...')
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchCoursesData()
+  })
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4">
+      {courses.map((course, index) => (
+        <div key={index} className={``}>
+          <CourseCard
+            bookCover={course.bookCover}
+            bookTitle={course.bookTitle}
+            author={course.author}
+            rating={course.rating}
+            price={course.price}
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ArchiCourses;
